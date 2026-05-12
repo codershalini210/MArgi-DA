2b9954fc9ac8100f85a2543ffc30bce519fdb17b
shalini <s.rathore43@gmail.com>
2026-05-12 11:41:03 +0530

    sdfsd

---
+++ functions/types.py
@@ -0,0 +1,12 @@
+# def result(name,marks):
+#     if(marks>=35):
+#         print(name ," result: PASS")
+#     else:
+#         print(name, "result :FAIL")
+# result("Ron",45)
+# result(marks=25,name="Maria")
+def user(username,nationality="Indian"):
+    print(username ," is ",nationality)
+
+user("Ron","American")
+user("Raman")
