becee9dc005f38cbbbf71309845ef91e9ef67b70
shalini <s.rathore43@gmail.com>
2026-05-12 10:46:38 +0530

    sasas

--- functions/intro.py
+++ functions/intro.py
@@ -1,17 +1,63 @@
-# def hello():
-#     print("hello world ")
-# hello()
-# hello()
-# print("----------------------")
-# hello()
-# -----------------------------
-# def welcome(n):
-#     print("welcome ",n)
-# welcome("John")
-# welcome("Ron")
-# ---------------------------
-def add(a,b):
-    c = a+b
-    print("sum is ",c)
-add(12,3)
-add(21,32)
+# Create a function that accepts a string and prints its length.
+# def strlen(s):
+#     print("length of ",s ," is ", len(s))
+# strlen("Hello world ")
+
+# Write a function that counts the number of vowels in a word.
+# def countvowel(w):
+#     count = 0 
+#     for l in w:
+#         if(l in "AEIOUaeiou"):
+#             count=count+1
+#     print("Total nof of vowels in ", w," are ",count)
+# countvowel("hello world how are you today")
+# Create a function that takes a list of numbers and prints the maximum value.
+# def maxlist(lst):
+#     print("highest no of of list is ", max(lst))
+# maxlist([32,12,43,542,234,12])
+# Write a function that returns the cube of a number.
+
+# Create a function to calculate the factorial of a number.
+# def fact(n):
+#     factorial = 1
+#     for i in range(1,n+1):
+#         factorial = factorial*i 
+#     print("Factorial of ",n ," is ",factorial)
+# fact(5)
+# fact(6)
+# Write a function that checks whether a number is prime or not.
+# def chkprime(n):
+#     msg = " is prime"
+#     for i in range(2,n):
+#         if(n/i==0):
+#             msg = " is not prime"
+#             break
+    
+#     print(n,msg)
+# chkprime(5)
+# chkprime(15)
+# Create a function that prints the multiplication table of a given number.
+
+
+
+# # # def hello():
+# # #     print("hello world ")
+# # # hello()
+# # # hello()
+# # # print("----------------------")
+# # # hello()
+# # # -----------------------------
+# # # def welcome(n):
+# # #     print("welcome ",n)
+# # # welcome("John")
+# # # welcome("Ron")
+# # # ---------------------------
+# # def add(a,b):
+# #     c = a+b
+# #     print("sum is ",c)
+# # add(12,3)
+# # add(21,32)
+
+# # def eligible(name,age):
+
+# # eligible("john",65)
