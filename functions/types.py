da219bb07dee0fce3836b636f31e130b71d7366c
shalini <s.rathore43@gmail.com>
2026-05-13 10:52:34 +0530

    sdfsd

--- functions/types.py
+++ functions/types.py
@@ -1,12 +1,33 @@
-# def result(name,marks):
-#     if(marks>=35):
-#         print(name ," result: PASS")
-#     else:
-#         print(name, "result :FAIL")
-# result("Ron",45)
-# result(marks=25,name="Maria")
-def user(username,nationality="Indian"):
-    print(username ," is ",nationality)
+def chkeven(n):
+    if(n%2 ==0):
+        return True
+    return False
+if(chkeven(50)):
+    print("even no ")
+else:
+    print ("odd no ")
+# print("5 is even no ", chkeven(5))
+# print("8 is even no ", chkeven(8))
 
-user("Ron","American")
-user("Raman")
+# def calculate(a,b):
+#     c = a+b
+#     d = a-b
+#     return c,d
+
+# x,y = calculate(12,2)
+# print(x,y)
+
+
+# def square(n):
+#     s = n*n
+#     return s
+# x = square(2)
+# print("square of 2 is",x )
+# print("square of 5 is ", square(5))
+# ....................................
+# def marks(*n):
+#     print("total marks ", sum(n))
+#     print("avg marks", sum(n)/len(n))
+# marks(12,32,23,43,42)
+# marks(36,66,45)
+
