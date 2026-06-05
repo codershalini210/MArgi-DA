7d38701c5c18468e5ab35765165baf0d0828e879
shalini <s.rathore43@gmail.com>
2026-06-05 09:58:35 +0530

    array in numpy

---
+++ numpypractice/array3.py
@@ -0,0 +1,34 @@
+import numpy as np 
+# ---------------
+# ary = np.full(5,100)
+# ary = np.full((5,3),100)
+#  this will create a 2d aryy of 5R and 3C
+# print(ary)
+# ---------------
+# aryrange = np.arange(5)
+# print(aryrange)
+# aryrange =np.arange(12,18)
+# print(aryrange)
+# aryrange =np.arange(1,10,3)
+# print(aryrange)
+# ---------------
+# arylin = np.linspace(0,10,5)
+# print(arylin)
+
+# arylin = np.linspace(10,20,3)
+# print(arylin)
+
+# arylin= np.linspace(20,5,3)
+# print(arylin)
+# ---------------------
+# aryi = np.eye(4)
+# print(aryi)
+# aryi = np.eye(4,3)
+# print(aryi)
+# ----------------------
+aryrandom = np.random.randint(25,50,10)
+print(aryrandom)
+arr = np.random.rand(15)
+
+print(arr)
+
